36665cb5af96e42295f64de1cb84dcdcb2cd5ff1
Mirte Theunis <mirte.theunis@student.thomasmore.be>
2025-05-28 16:22:45 +0200

    add line animation TodoItem

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,9 +1,30 @@
-import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, Pressable, StyleSheet, Text, View } from 'react-native';
 import theme from '../theme';
 
 export default function TodoItem({ text }) {
+    // Adds a checkmark when clicked and a animated line trough the text
+
     const [completed, setCompleted] = useState(false);
+    const animation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(animation, {
+        toValue: completed ? 1 : 0,
+        duration: 300,
+        useNativeDriver: true,
+        }).start();
+    }, [completed]);
+
+    const scaleX = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1], 
+    });
+
+    const translateX = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [24, 0], 
+    });
 
     return (
         <View style={styles.container}>
@@ -12,7 +33,18 @@ export default function TodoItem({ text }) {
             {completed && <Text style={styles.checkmark}>✓</Text>}
             </View>
         </Pressable>
-        <Text style={[theme.fonts.body, completed && styles.textCompleted]}>{text}</Text>
+        <View style={styles.textWrapper}>
+            <Text style={[theme.fonts.body, completed && styles.textCompleted]}>{text}</Text>
+            <Animated.View
+            style={[
+                styles.strikeThrough,
+                {
+                transform: [{ scaleX }, { translateX }],
+                opacity: animation,
+                },
+            ]}
+            />
+        </View>
         </View>
     );
 }
@@ -46,8 +78,18 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     fontWeight: 'bold',
   },
+  textWrapper: {
+    position: 'relative',
+  },
   textCompleted: {
     color: '#1C2133B3',
-    textDecorationLine: 'line-through',
+  },
+  strikeThrough: {
+    position: 'absolute',
+    height: 2,
+    backgroundColor: '#1C2133B3',
+    top: '50%',
+    left: 0,
+    right: 0,
   },
 });
